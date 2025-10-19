2f3c072ee0ed0dc1cf94252d7d34fc7d3fa431dd
masb0ymas <n.fajri@outlook.com>
2025-10-19 14:28:51 +0700

    fix: update docker compose & env

--- secret.sh
+++ secret.sh
@@ -15,17 +15,13 @@ if [ -f "$BASE_ENV_FILE" ]; then
   echo " - Copy $BASE_ENV_FILE to $ENV_FILE"
 
   if [ "$OS_TYPE" = "macos" ]; then
-    # Generate a secure DEFAULT_PASS
-    sed -i '' "s|^DEFAULT_PASS=.*|DEFAULT_PASS='$(openssl rand -hex 16)'|" "$ENV_FILE"
+    # Generate a secure DB_PASSWORD
+    sed -i '' "s|^DB_PASSWORD=.*|DB_PASSWORD='$(openssl rand -hex 16)'|" "$ENV_FILE"
 
-    # Generate a secure AUTH_SECRET
-    sed -i '' "s|^AUTH_SECRET=.*|AUTH_SECRET='$(openssl rand -base64 32)'|" "$ENV_FILE"
   elif [ "$OS_TYPE" = "ubuntu" ]; then
-    # Generate a secure DEFAULT_PASS
-    sed -i "s|^DEFAULT_PASS=.*|DEFAULT_PASS='$(openssl rand -hex 16)'|" "$ENV_FILE"
+    # Generate a secure DB_PASSWORD
+    sed -i "s|^DB_PASSWORD=.*|DB_PASSWORD='$(openssl rand -hex 16)'|" "$ENV_FILE"
 
-    # Generate a secure AUTH_SECRET
-    sed -i "s|^AUTH_SECRET=.*|AUTH_SECRET='$(openssl rand -base64 32)'|" "$ENV_FILE"
   fi
 
   echo "Secrets have been generated and saved to .env file"
